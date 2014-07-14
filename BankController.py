5e1690d3f441fe44ea755296d5edbe781b9913e9
Jinjing Liang <JinjingLiang@JinjingtekiMacBook-Pro.local>
2014-07-14 11:49:55 +0800

    fixed BankController Error

--- BankController.py
+++ BankController.py
@@ -38,14 +38,14 @@ class BankController():
         print "The interest rate for your saving is %.5f " %interest_rate  #print out 5 effective digits
         wanna_save = check_confirm("Print Y to continue your transfering money to your saving account with above interest rate. Press N to exit.") 
         if wanna_save : 
-            amount = self.bank.money  #initialize amount to enter 
-            while amount > self.bank.checking_account.balance : 
+            amount = self.bank.money  #initialize amount to enter with a large number to make sure we'll be in while loop
+            while amount > self.player.checking_account.balance : 
                 amount = check_positive_valid_input("Please enter the amount you want to loan from the bank ? ")
-                if amount < self.bank.checking_account.balance:
+                if amount < self.player.checking_account.balance:
                     break
                 print "Please enter the right input."
-            self.bank.checking_account..withdraw(amount) #first, withdraw the money from checkign account
-            self.bank.saving_account.save(amount) #then put it in saving account 
+            self.player.checking_account.withdraw(amount) #first, withdraw the money from checking account
+            self.player.saving_account.save(amount) #then put it in saving account 
             G.tillWithdraw = time   #set the countdown
             self.bank.interest = amount * interest_rate
         else : 
@@ -58,9 +58,9 @@ class BankController():
             if not go_on :
                 return
             self.bank.interest = 0
-        to_checking = self.bank.saving_account.balance + self.bank.interest   #initial money plus interest 
-        self.bank.saving_account..withdraw(self.bank.saving_account.balance) #first, withdraw the money from saving account
-        self.bank.checking_account.save(to_checking) #then put it in checking account 
+        to_checking = self.player.saving_account.balance + self.bank.interest   #initial money plus interest 
+        self.player.saving_account.withdraw(self.player.saving_account.balance) #first, withdraw the money from saving account
+        self.player.checking_account.save(to_checking) #then put it in checking account 
 
 
 
@@ -80,44 +80,44 @@ class BankController():
             amount = check_positive_valid_input("Please enter the amount you want to loan from the bank ? ")
             G.tillpay = time*4 #set the countdown to pay the money 
             self.player.loan = amount
-            slef.bank.checking_account.save(amount) #transfer the money to player's checking account
-            print "Sucessfully borrow %d dollars from the bank. You have to pay it back" %amount
+            self.player.checking_account.save(amount) #transfer the money to player's checking account
+            print "Sucessfully borrow %d dollars from the bank. You have to pay it back %d months later." %(amount,G.tillpay)
         else : 
             return
 
 
     def pay_loan(self):
-        if not (self.bank.checking_account.balance<self.player.loan ) :
+        if not (self.player.checking_account.balance<self.player.loan ) :
             print "Paying back all the %d dollars loan using your checking account " %self.player.loan
-            self.bank.checking_account.withdraw(self.player.loan )  #pay back the loan by withdrawing money from checking account
+            self.player.checking_account.withdraw(self.player.loan )  #pay back the loan by withdrawing money from checking account
             return
-        if not (self.bank.checking_account.balance + self.bank.saving_account.balance < self.player.loan ):
-            print "Paying back the %d dollars of your totoal %d loan using your checking account. " %(self.bank.checking_account,self.player.loan) 
-            self.bank.checking_account.withdraw(self.bank.checking_account.balance) #first, use all money in checking account 
+        if not (self.player.checking_account.balance + self.player.saving_account.balance < self.player.loan ):
+            print "Paying back the %d dollars of your totoal %d loan using your checking account. " %(self.player.checking_account,self.player.loan) 
+            self.player.checking_account.withdraw(self.player.checking_account.balance) #first, use all money in checking account 
             print "Paying back the remaining %d dollars of loan using your saving account. " %(self.player.loan) 
-            self.bank.loan  -= self.bank.checking_account.balance  #deduct the paid part from self.player.loan 
-            self.bank.saving_account.withdraw(self.bank.loan ) # next, pay the rest of the money using the money in saving account
+            self.player.loan  -= self.player.checking_account.balance  #deduct the paid part from self.player.loan 
+            self.player.saving_account.withdraw(self.player.loan ) # next, pay the rest of the money using the money in saving account
             return
         #has not enough money to pay.
         else :
             orginial_loan = self.player.loan
-            print "Paying back the %d dollars of your totoal %d loan using your checking account. " %(self.bank.checking_account.balance,self.player.loan) 
-            self.player.loan  -= self.bank.checking_account.balance #deduct the paid part from self.player.loan 
+            print "Paying back the %d dollars of your totoal %d loan using your checking account. " %(self.player.checking_account.balance,self.player.loan) 
+            self.player.loan  -= self.player.checking_account.balance #deduct the paid part from self.player.loan 
             self.player.checking_account.withdraw(self.player.checking_account.balance) #first, use all money in checking account 
-            print "Paying back the %d dollars of the remaining %d loan using your saving account. " %(self.bank.saving_account.balance,self.player.loan) 
-            self.player.loan -= self.bank.saving_account.balance  #deduct the paid part from self.player.loan 
-            self.bank.saving_account.withdraw(self.bank.saving_account_balance)# next, pay the rest of the money using the money in saving account
+            print "Paying back the %d dollars of the remaining %d loan using your saving account. " %(self.player.saving_account.balance,self.player.loan) 
+            self.player.loan -= self.player.saving_account.balance  #deduct the paid part from self.player.loan 
+            self.player.saving_account.withdraw(self.player.saving_account_balance)# next, pay the rest of the money using the money in saving account
             deduct = 100*(self.player.loan/orginial_loan)
-            print "You don't have enough money to pay back your loan. As a result, we deduct %d points from your credit points") %deduct
+            print "You don't have enough money to pay back your loan. As a result, we deduct %d points from your credit points" %deduct
             self.player.credit -= deduct   #deduct credit based on how much you couldn't repaly
 
         
         def check_checking_balance(self):
-            print "Your checking account balance is %d dollars. " %self.bank.checking_account.balance 
+            print "Your checking account balance is %d dollars. " %self.player.checking_account.balance 
 
 
         def check_saving_balance(self):
-            print "Your saving account balance is %d dollars. " %self.bank.saving_account.balance       
+            print "Your saving account balance is %d dollars. " %self.player.saving_account.balance       
         
 
 
